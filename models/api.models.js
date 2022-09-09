868866207efce7b47a300bcb3864599d2ccaf61c
cremedelakremlin <michael.leversha@gmail.com>
2022-09-09 16:14:43 +0100

    adds /api endpoints

--- models/api.models.js
+++ models/api.models.js
@@ -2,5 +2,5 @@ const endpoints = require('../endpoints.json')
 
 exports.selectApi = () => {
     const endpoints = endpoints
-    res.status(200).send({endpoints})
+    res.status(200).send(endpoints)
  };
